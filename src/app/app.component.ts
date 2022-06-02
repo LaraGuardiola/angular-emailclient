3959edebf44a6da75871912e82b5d42202a77fab
Sergio <webdev.slara@gmail.com>
2022-06-02 22:36:49 +0200

    dunno what to do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   resize() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    console.log(vh * 100)
   }
 
   ngOnInit() {window.addEventListener('load', this.resize)}
