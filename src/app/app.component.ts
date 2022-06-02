ffa97e924ab692b4747791c71b85e2eb0aedd10b
Sergio <webdev.slara@gmail.com>
2022-06-02 22:39:01 +0200

    another try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   resize() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.body.style.setProperty('height', `${vh * 100}px`);
     console.log(vh * 100)
   }
 
