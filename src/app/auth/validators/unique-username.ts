9a3eea726e85dca49717836fd01b5c69e6efbc26
Sergio <webdev.slara@gmail.com>
2022-06-02 17:41:22 +0200

    added async validate methods

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { AbstractControl, AsyncValidator, FormControl, ValidationErrors } from '@angular/forms'
+import { AbstractControl, AsyncValidator } from '@angular/forms'
+import { map, catchError } from 'rxjs/operators'
+import { of } from 'rxjs'
 
 @Injectable({ providedIn: 'root'})
 export class UniqueUsername  implements AsyncValidator {
     constructor(private http: HttpClient){}
 
-    validate(control: AbstractControl) {
+    validate = (control: AbstractControl) =>{
         const { value } = control
-        console.log(value)
-        return new Promise<ValidationErrors>((resolve, reject) => {}) || null
+        const res = this.http.post<any>('https://api.angular-email.com/auth/username',{
+            username: value
+        }).pipe(
+            map(() => {
+                return null
+            }),
+            catchError((err) => {
+                //of operator is a shortcut for creating a new Observable
+                if(err.error.username){
+                    return of({ nonUniqueUsername: true })
+                }else{
+                    return of({ noConnection: true})
+                }
+            })
+        )
+        return res
     }
 }
